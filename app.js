8e51663e93d8bdca6cfd7ebf0fb886a4369ed125
Naimul Islam <ms0413354@gmail.com>
2025-12-01 23:01:19 +0600

    add centerlize error middleware

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ import cors from 'cors';
 import { config } from 'dotenv';
 
 export const app = express();
-config({ path: './config.env' });
+config();
+import pool from './config/db.js';
+import { errorMiddleWare } from './middlewares/errorHandler.js';
+
 app.use(
 	cors({
 		origin: [process.env.FRONTEND_URL],
@@ -15,3 +18,11 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+// CONNECT DATABASE
+app.get('/', async (req, res) => {
+	const result = await pool.query('SELECT current_database()');
+	res.send(`The databse name is: ${result.rows[0].current_database}`);
+});
+
+app.use(errorMiddleWare);
